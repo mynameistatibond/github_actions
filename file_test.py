c0e08983567adff9c14dd7a7946f72f6c059149f
Ubuntu <ubuntu@ip-172-31-24-165.eu-west-1.compute.internal>
2025-10-08 18:05:25 +0000

    Add pytest + flake8 CI workflow and sample tests

---
+++ file_test.py
@@ -0,0 +1,21 @@
+"""Tiny unit tests just to demo GitHub Actions with pytest."""
+
+def test_calc_addition():
+    assert 2 + 4 == 6
+
+
+def test_calc_subtraction():
+    """Check subtraction 2 - 4."""
+    assert 2 - 4 == -2
+
+
+def test_calc_multiply():
+    """Check multiplication 2 * 4."""
+    assert 2 * 4 == 8
+
+
+def test_hello():
+    """Return value equals 'hello'."""
+    output = "hello"
+    assert output == "hello"
+
